6e698179100f673b8a682df56cfd6098587e1dfc
Farkhod <farxotbekturobidinov43@gmail.com>
2024-02-26 21:45:56 +0300

    Рефакторинг главной страницы

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,26 +21,23 @@
                     @foreach($posts as $post)
                         <div class="blog-post-thumb">
                             <div class="blog-post-image">
-                                <a href="{{ route('posts.show', [$post]) }}">
-                                    <img src="{{ asset('storage/' . '/posts/posters/' . $post["poster"]) }}"
+                                <a href="{{ route('posts.show', $post->id) }}">
+                                    <img src="{{ asset('storage/posts/posters/' . $post->poster) }}"
                                          class="img-responsive img-responsive--dimensions" alt="Blog Image">
                                 </a>
                             </div>
                             <div class="blog-post-title">
-                                <h3><a href="{{ route('posts.show', [$post]) }}">{{ $post->name }}</a></h3>
+                                <h3><a href="{{ route('posts.show', $post->id) }}">{{ $post->name }}</a></h3>
                             </div>
                             <div class="blog-post-format">
-                                <span><a href="#"><img src="
-{{--                                images/author-image1.jpg--}}
-                                {{ asset('assets/images/author-image1.jpg') }}
-                                " class="img-responsive img-circle"> Jen Lopez</a></span>
+                                <span><a href="#"><img src="{{ asset('assets/images/author-image1.jpg') }}"
+                                class="img-responsive img-circle"> Jen Lopez</a></span>
                                 <span><i class="fa fa-date"></i>{{ $post->created_at }}</span>
-                                <span><a href="#"><i class="fa fa-comment-o"></i> 35 Comments</a></span>
+                                <span><i class="fa fa-comment-o"></i> 35 Comments</span>
                             </div>
                             <div class="blog-post-des">
                                 <p>{{ $post->description }}</p>
-                                <a href="{{ route('posts.show', [$post]) }}" class="btn btn-default">Continue
-                                    Reading</a>
+                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default">Читать далее...</a>
                             </div>
                         </div>
 
