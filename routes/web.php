2e16093699dc40ad88ad3cff6bccdb6df5ec6db8
Farkhod <farxotbekturobidinov43@gmail.com>
2024-03-25 21:51:38 +0300

    Вернули роуты и защитили их авторизацией

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +29,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('comments', CommentController::class)->only(['store', 'destroy']);
 });
-
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('posts', PostController::class)->only(['create','store', 'destroy']);
+    Route::resource('categories', CategoryController::class)->only(['create','store', 'destroy']);
+});
+    Route::resource('posts', PostController::class)->only(['index', 'show']);
+    Route::resource('categories', CategoryController::class)->only(['index', 'show']);
 require __DIR__.'/auth.php';
