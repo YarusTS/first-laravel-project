e1b0b403a68a936312851e164a5064e39424b775
Farkhod <farxotbekturobidinov43@gmail.com>
2024-04-08 21:49:54 +0300

    Добавили поле slug в таблицу постов(1)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,54 +1,3 @@
-{{--@extends('layouts.app')--}}
-
-{{--@section('content')--}}
-{{--    <section id="home" class="main-home parallax-section">--}}
-{{--        <div class="overlay"></div>--}}
-{{--        <div id="particles-js"></div>--}}
-{{--        <div class="container">--}}
-{{--            <div class="row">--}}
-
-{{--                <div class="col-md-12 col-sm-12">--}}
-{{--                    <h1>Hello! This is Neuron.</h1>--}}
-{{--                    <h4>Responsive Blog HTML CSS Website Template</h4>--}}
-{{--                    <a href="#blog" class="smoothScroll btn btn-default">Discover More</a>--}}
-{{--                </div>--}}
-
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </section>--}}
-
-{{--    <section id="blog">--}}
-{{--        <div class="container">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-md-offset-1 col-md-10 col-sm-12">--}}
-{{--                @foreach($posts as $post)--}}
-{{--                        <div class="blog-post-thumb">--}}
-{{--                            <div class="blog-post-image">--}}
-{{--                                <a href="{{ route('posts.show', $post->id) }}">--}}
-{{--                                    <img style="width: 50%; height: 30%" src="{{ asset('storage/posts/posters/' . $post->poster) }}"--}}
-{{--                                     class="img-responsive" alt="Blog Image">--}}
-{{--                                </a>--}}
-{{--                            </div>--}}
-{{--                            <div class="blog-post-title">--}}
-{{--                                <h3><a href="{{ route('posts.show', $post->id) }}">{{ $post->name }}</a></h3>--}}
-{{--                            </div>--}}
-{{--                            <div class="blog-post-format">--}}
-{{--                                <span><a href="#"><img src="{{ asset('assets/images/author-image1.jpg') }}" class="img-responsive img-circle" alt="Картинка автора"> Jen Lopez</a></span>--}}
-{{--                                <span><i class="fa fa-date"></i>{{$post->created_at->translatedFormat('d F, Y')}}</span>--}}
-{{--                                <span><a href="#content"><i class="fa fa-comment-o"></i>{{ trans_choice(':count комментарий|:count комментария|:count комментариев', $post->comments->count())}}</a></span>--}}
-{{--                            </div>--}}
-{{--                            <div class="blog-post-des">--}}
-{{--                                <p>{{ $post->description }}</p>--}}
-{{--                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default">Читать далее</a>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                @endforeach--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </section>--}}
-{{--@endsection--}}
-
 @extends('layouts.app')
 
 @section('content')
@@ -72,13 +21,13 @@
                     @foreach($posts as $post)
                         <div class="blog-post-thumb">
                             <div class="blog-post-image">
-                                <a href="{{ route('posts.show', $post->id) }}">
+                                <a href="{{ route('posts.show', $post->slug) }}">
                                     <img style="width: 50%; height: 30%" src="{{ asset('storage/posts/posters/' . $post->poster) }}"
                                          class="img-responsive img-responsive--dimensions" alt="Blog Image">
                                 </a>
                             </div>
                             <div class="blog-post-title">
-                                <h3><a href="{{ route('posts.show', $post->id) }}">{{ $post->name }}</a></h3>
+                                <h3><a href="{{ route('posts.show', $post->slug) }}">{{ $post->name }}</a></h3>
                             </div>
                             <div class="blog-post-format">
                                 <span><a href="#"><img src="{{ asset('assets/images/author-image1.jpg') }}"
@@ -88,7 +37,7 @@
                             </div>
                             <div class="blog-post-des">
                                 <p>{{ $post->description }}</p>
-                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default">Читать далее...</a>
+                                <a href="{{ route('posts.show', $post->slug) }}" class="btn btn-default">Читать далее...</a>
                             </div>
                         </div>
 
